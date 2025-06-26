8424ab4f152747b73b51915abee35af3065fb786
vHrqO <120627599+vHrqO@users.noreply.github.com>
2025-06-27 01:27:54 +0800

    Add - session_meta_repo

---
+++ matcha/lib/repository/session_meta_repo.dart
@@ -0,0 +1,54 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import 'package:matcha/database/database.dart' as database;
+import 'package:matcha/model/session/session_meta.dart';
+import 'package:matcha/view_model/shared/app_viewmodel.dart';
+
+part 'session_meta_repo.g.dart';
+
+@riverpod
+class SessionMetaRepo extends _$SessionMetaRepo {
+  // getAll()
+  @override
+  Future<List<SessionMeta>> build() async {
+    final appDb = ref.watch(appDbProvider);
+
+    final List<database.SessionData> queryResult = await appDb
+        .select(appDb.session)
+        .get();
+
+    final List<SessionMeta> data = queryResult
+        .map((item) => SessionMeta(id: item.id, name: item.name))
+        .toList();
+
+    return data;
+  }
+
+  Future<void> add(String name) async {
+    final appDb = ref.read(appDbProvider);
+
+    await appDb
+        .into(appDb.session)
+        .insert(database.SessionCompanion.insert(name: name));
+
+    ref.invalidateSelf();
+  }
+
+  Future<void> updateData(SessionMeta sessionMeta) async {
+    final appDb = ref.read(appDbProvider);
+
+    await appDb
+        .update(appDb.session)
+        .replace(database.SessionData(id: sessionMeta.id, name: sessionMeta.name));
+
+    ref.invalidateSelf();
+  }
+
+  Future<void> delete(int id) async {
+    final appDb = ref.read(appDbProvider);
+
+    await (appDb.delete(appDb.session)..where((t) => t.id.equals(id))).go();
+
+    ref.invalidateSelf();
+  }
+}
