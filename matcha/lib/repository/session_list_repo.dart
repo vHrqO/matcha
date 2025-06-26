5a967e82bd77e92507c982d15f638993bec394d8
vHrqO <120627599+vHrqO@users.noreply.github.com>
2025-06-27 02:11:50 +0800

    rename

--- matcha/lib/repository/session_list_repo.dart
+++ matcha/lib/repository/session_list_repo.dart
@@ -4,10 +4,10 @@ import 'package:matcha/database/database.dart' as database;
 import 'package:matcha/model/session/session_meta.dart';
 import 'package:matcha/view_model/shared/app_viewmodel.dart';
 
-part 'session_meta_repo.g.dart';
+part 'session_list_repo.g.dart';
 
 @riverpod
-class SessionMetaRepo extends _$SessionMetaRepo {
+class SessionListRepo extends _$SessionListRepo {
   // getAll()
   @override
   Future<List<SessionMeta>> build() async {
