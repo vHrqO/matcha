8b1d38a00c3d59940274c1700df7e8b1f16af671
vHrqO <120627599+vHrqO@users.noreply.github.com>
2025-06-28 18:34:36 +0800

    Refactor – use real database repo implementation

--- matcha/lib/view_model/tab_manager/session_list_viewmodel.dart
+++ matcha/lib/view_model/tab_manager/session_list_viewmodel.dart
@@ -1,57 +1,33 @@
+import 'package:matcha/repository/session_list_repo.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import 'package:matcha/model/session/session_meta.dart';
 import 'package:matcha/mock_data/session_list.dart';
 import 'package:matcha/view_model/shared/selected_viewmodel.dart';
 import 'package:matcha/app/constants.dart';
+import 'package:collection/collection.dart';
 
 part 'session_list_viewmodel.g.dart';
 
-// to_do: rename to sessionMetaList?
 @riverpod
 class SessionList extends _$SessionList {
-  List<SessionMeta>? _sessionList;
-
   @override
   Future<List<SessionMeta>> build() async {
-    // from database
-    _sessionList ??= mockSessionList1;
+    final sessionList = await ref.watch(sessionListRepoProvider.future);
 
-    return _sessionList!;
-  }
-
-  Future<void> updateSessionList(List<SessionMeta> sessions) async {
-    // to database
-
-    //
-    state = AsyncData(sessions);
+    return sessionList;
   }
 
   Future<void> addSession(String name) async {
-    if (_sessionList == null) {
-      throw Exception('Sessions not initialized');
-    }
-
-    // to database
-    final lastId = _sessionList!.isNotEmpty ? _sessionList!.last.id : 0;
-    _sessionList!.add(SessionMeta(id: lastId + 1, name: name));
-
-    //
-    state = AsyncData(_sessionList!);
+    await ref.read(sessionListRepoProvider.notifier).add(name);
   }
 
   Future<void> deleteSession(int sessionId) async {
-    if (_sessionList == null) {
-      throw Exception('Sessions not initialized');
-    }
+    await ref.read(sessionListRepoProvider.notifier).delete(sessionId);
+  }
 
-    // to database
-    _sessionList!.removeWhere((session) => session.id == sessionId);
-
-    // reset selected session id
-    ref.invalidate(selectedSessionIdProvider);
-
-    state = AsyncData(_sessionList!);
+  Future<void> reorder(int oldIndex, int newIndex) async {
+    await ref.read(sessionListRepoProvider.notifier).reorder(oldIndex, newIndex);
   }
 }
 
